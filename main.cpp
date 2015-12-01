208bd208e135e08be48d5826d50ee7dbd95a0880
Ben Holden <bholden1@andrew.cmu.edu>
2015-12-01 11:48:23 -0500

    Added a cool #define

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+#define PRINT_VARIABLE(x) std::cout << #x << ": " << x << std::endl;
+
 extern "C" float add101(float a)
 {
 	return 101.0+a;
@@ -7,5 +9,7 @@ extern "C" float add101(float a)
 
 int main(int argc, char **argv) {
     std::cout << "Hello, world!" << std::endl;
+    double cheese = 0.12345;
+    PRINT_VARIABLE(cheese);
     return 0;
 }
